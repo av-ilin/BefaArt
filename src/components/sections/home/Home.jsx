2e365408d8c18a56860f5579f0f9ba593a26f163
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-27 15:14:28 +0300

    upd::home

--- src/components/sections/home/Home.jsx
+++ src/components/sections/home/Home.jsx
@@ -3,7 +3,15 @@ import styles from "./Home.module.css";
 const Home = () => {
     return (
         <div className={styles.home}>
-            <div className={styles.container}>asdasd</div>
+            <div className={styles.container}>
+                <div className={styles.hello}>
+                    <h6>Hello, i am</h6>
+                    <h1>BefaArt.</h1>
+                </div>
+                <img src="images/home/home-art01.png" alt="art" />
+                <img src="images/home/home-art02.png" alt="art" />
+                <img src="images/home/home-art03.png" alt="art" />
+            </div>
         </div>
     );
 };
