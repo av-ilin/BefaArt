2722ac89686a40b55eba2ee40efc286b6e9919b5
Андрей Ильин <a.ilin.v@yandex.ru>
2023-03-28 22:05:53 +0300

    upd::about

--- src/components/sections/about/About.jsx
+++ src/components/sections/about/About.jsx
@@ -1,7 +1,24 @@
 import styles from "./About.module.css";
 
+console.log(styles);
+
 const About = () => {
-    return <div className={styles.about + " section"}>inin</div>;
+    return (
+        <div className={styles.about + " section"}>
+            <div className={styles.box}>
+                <img
+                    className={styles.myphoto}
+                    src="images/me.png"
+                    alt="my photo"
+                />
+                <p className={styles.aboutme}>
+                    I, <span>Befa</span>, put mine artistic abstracts Works
+                    before. These art pictures show the whole spectrum of my
+                    inner joy, spiritual Mood, immeasurable Joy for life.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default About;
